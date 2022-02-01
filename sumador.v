e14cad78cc92a396ffed78bc92f479a34ba0801e
Edwin Villa <e28v11a96@gmail.com>
2022-02-01 15:35:54 -0600

    sumador

---
+++ sumador.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:35:00 02/01/2022 
+// Design Name: 
+// Module Name:    sumador 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sumador(
+    input [13:0] a,
+    input [13:0] b,
+    output [13:0] c
+    );
+	 
+	 always @(a, b)
+		c = a + b;
+
+
+endmodule
