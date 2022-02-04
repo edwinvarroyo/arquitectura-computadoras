b059a6800250c44e2a2460f445ade441c36bca1a
Edwin Villa <e28v11a96@gmail.com>
2022-02-04 13:45:56 -0600

    fix data assign

--- memoria.v
+++ memoria.v
@@ -21,7 +21,6 @@
 module memoria(
     input clk,
     input en,
-    input wr,
     input [4:0] address,
     input [13:0] datain,
     output reg [13:0] dataout
@@ -30,22 +29,12 @@ module memoria(
 	 reg [13:0] mem [31:0];
 	 initial $readmemh("memoria.txt",mem);
 	 
-	 always @( posedge clk, en, wr, address, datain)
-	 begin
+	 assign dataout = mem[add];
 	 
+	 always @( posedge clk)
+	 begin
 		if(en)
-		begin
-		
-			if(wr)
-			begin
-				mem[address] = datain;
-			end
-			if(!wr)
-			begin
-				dataout = mem[address];
-			end
-			
-		end
+			mem[address] = datain;
 	 end
 
 
